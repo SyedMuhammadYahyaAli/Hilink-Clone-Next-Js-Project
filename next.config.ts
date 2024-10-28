22a9ecf57d196f60223ede480d7bb6ab87724d2c
SyedMuhammadYahyaAli <137400984+SyedMuhammadYahyaAli@users.noreply.github.com>
2024-10-28 16:22:00 +0500

    first Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
-
-export default nextConfig;
